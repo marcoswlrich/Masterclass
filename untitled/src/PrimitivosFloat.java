fd4b3360601db6d0b274e4335a13476e79bb2bd9
marcoswlrich <marcosapw.dev@gmail.com>
2022-09-20 02:28:26 -0400

    Tipos Primitivos float

---
+++ untitled/src/PrimitivosFloat.java
@@ -0,0 +1,9 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+        float realFloat = 1.5e4f; //15000f
+        System.out.println("realFloat = " + realFloat);
+
+        float negFloat = 0.00000000015f; //1.5e-10f anotacao cientfica
+        System.out.println("negFloat = " + negFloat);
+    }
+}
