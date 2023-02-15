7fdfcb68cd5c5413b64912b4f68b86b5bde31f0e
marcoswlrich <marcosapw.dev@gmail.com>
2023-02-15 00:07:09 -0400

    Sistema Numericos

--- untitled/src/SistemasNumericos.java
+++ untitled/src/SistemasNumericos.java
@@ -1,20 +1,42 @@
+import javax.swing.*;
+
 public class SistemasNumericos {
     public static void main(String[] args) {
-        int numeroDecimal = 500;
+        String numeroStr = JOptionPane.showInputDialog(null, "Adicione um numero inteiro");
+        int numeroDecimal = 0;
+        try {
+            numeroDecimal = Integer.parseInt(numeroStr);
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Error! Deve adicionar um numero inteiro");
+            main(args);
+            System.exit(0);
+        }
+
+
         System.out.println("numeroDecimal = " + numeroDecimal);
 
-        System.out.println("numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        String resultBin = "numero binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+        System.out.println(resultBin);
 
         int numBinario = 0b111110100;
         System.out.println("numBinario = " + numBinario);
 
-        System.out.println("numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+        String resultOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println(resultOctal);
+
         int numOctal = 0764;
         System.out.println("numOctal = " + numOctal);
 
-        System.out.println("numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        String resultHex = "numero hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(resultHex);
 
         int numHex = 0x1f4;
         System.out.println("numHex = " + numHex);
+
+        String mensage = resultBin;
+        mensage += "\n" + resultOctal;
+        mensage += "\n" + resultHex;
+
+        JOptionPane.showMessageDialog(null, mensage);
     }
 }
